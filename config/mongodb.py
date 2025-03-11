206c9a2f7d65b9c3a7a21748748e1bcb342194cf
EmilianoChofis <20213tn128@utez.edu.mx>
2025-03-11 17:31:41 -0600

    change the conn variables to env

--- config/mongodb.py
+++ config/mongodb.py
@@ -3,6 +3,9 @@ mongodb.py
 """
 
 # Import the required libraries
+import os
+
+import dotenv
 from beanie import init_beanie
 
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -13,18 +16,23 @@ from models.box import Box
 from models.sheet import Sheet
 
 
+dotenv.load_dotenv()
+
+MONGODB_URL = os.getenv("MONGODB_URL")
+MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME")
+
 # Connect to the MongoDB
 async def init_db():
     """Initialize the MongoDB connection."""
     try:
         logger.info("Connecting to MongoDB...")
-        client = AsyncIOMotorClient("mongodb://localhost:27017")
+        client = AsyncIOMotorClient(MONGODB_URL)
 
         # Test the connection
         await client.admin.command("ping")
         logger.info("MongoDB connection successful")
 
-        db = client["moprocor"]
+        db = client[MONGODB_DB_NAME]
         await init_beanie(database=db, document_models=[Box,Sheet])
         logger.info("Database initialization complete.")
     except Exception as e:
