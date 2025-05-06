5c16ccccf216bc8ebe959f1a9df7e8cf447cc8b9
EmilianoChofis <20213tn128@utez.edu.mx>
2025-05-06 11:57:17 -0600

    documentacion a purchase router

--- api/routes/purchase_router.py
+++ api/routes/purchase_router.py
@@ -1,31 +1,52 @@
 """Define the purchase_router module"""
 
-import json
 from typing import List
 
-from fastapi import HTTPException, APIRouter, status, Query, Form, File, UploadFile
+from fastapi import HTTPException, APIRouter, status, Query
 
 from models.purchase import Purchase
 from services.purchase_service import PurchaseService
 
+# Create an instance of APIRouter to define the routes for the purchase module
 router = APIRouter()
-ITEMS_PER_PAGE = 10
+ITEMS_PER_PAGE = 10  # Default number of items per page for pagination
 
 
 @router.get("/getAll", response_model=List[Purchase])
 async def get_purchases():
-    """Define the get_purchases function"""
+    """
+    Retrieve all purchases.
+
+    Returns:
+        List[Purchase]: A list of all purchases.
+
+    Raises:
+        HTTPException: If an error occurs while retrieving purchases.
+    """
     try:
         return await PurchaseService.get_all_purchases()
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Failed to retrieve : {str(e)}",
+            detail=f"Error al cargas las ordenes de compra : {str(e)}",
         ) from e
 
 
 @router.get("/getByArapackLot", response_model=Purchase)
 async def get_purchase_by_id(arapack_lot: str):
+    """
+    Retrieve a purchase by its Arapack lot identifier.
+
+    Args:
+        arapack_lot (str): The Arapack lot identifier of the purchase.
+
+    Returns:
+        Purchase: The purchase corresponding to the given Arapack lot.
+
+    Raises:
+        HTTPException: If the Arapack lot is empty, the purchase is not found,
+                       or an error occurs while retrieving the purchase.
+    """
     try:
         if not arapack_lot:
             raise HTTPException(
@@ -48,10 +69,23 @@ async def get_purchase_by_id(arapack_lot: str):
 
 @router.get("/getFilteredPurchases", response_model=List[Purchase])
 async def get_filtered_purchases(
-    query: str = Query("", description="Filtro de búsqueda"),
-    page: int = Query(1, description="Número de página"),
+        query: str = Query("", description="Filtro de búsqueda"),
+        page: int = Query(1, description="Número de página"),
 ):
-    """Define the get_filtered_purchases function"""
+    """
+    Retrieve filtered purchases based on a search query and pagination.
+
+    Args:
+        query (str): The search filter query (default is an empty string).
+        page (int): The page number for pagination (default is 1).
+
+    Returns:
+        List[Purchase]: A list of filtered purchases.
+
+    Raises:
+        HTTPException: If the page number is less than 1 or an error occurs
+                       while retrieving the filtered purchases.
+    """
     if page < 1:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -66,25 +100,47 @@ async def get_filtered_purchases(
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Failed to retrieve filtered purchases: {str(e)}",
+            detail=f"Error al cargar las ordenes filtradas: {str(e)}",
         ) from e
 
 
 @router.get("/getPages", response_model=int)
 async def get_pages(query: str = Query("", description="Filtro de búsqueda")):
-    """Obtiene el total de páginas"""
+    """
+    Retrieve the total number of pages for a given search query.
+
+    Args:
+        query (str): The search filter query (default is an empty string).
+
+    Returns:
+        int: The total number of pages.
+
+    Raises:
+        HTTPException: If an error occurs while retrieving the total pages.
+    """
     try:
         return await PurchaseService.get_pages(query, ITEMS_PER_PAGE)
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Failed to retrieve total pages: {str(e)}",
+            detail=f"Error al cargar las páginas: {str(e)}",
         ) from e
 
 
 @router.post("/create", response_model=Purchase, status_code=status.HTTP_201_CREATED)
 async def create_purchase(purchase: Purchase):
-    """Define the create_purchase function"""
+    """
+    Create a new purchase.
+
+    Args:
+        purchase (Purchase): The purchase data to be created.
+
+    Returns:
+        Purchase: The created purchase.
+
+    Raises:
+        HTTPException: If an error occurs while creating the purchase.
+    """
     try:
         return await PurchaseService.create_purchase(purchase)
     except Exception as e:
@@ -96,7 +152,15 @@ async def create_purchase(purchase: Purchase):
 
 @router.get("/getNullDeliveryDates", response_model=List[Purchase])
 async def get_null_delivery_dates():
-    """Define the get_null_delivery_dates function"""
+    """
+    Retrieve purchases with null delivery dates.
+
+    Returns:
+        List[Purchase]: A list of purchases with null delivery dates.
+
+    Raises:
+        HTTPException: If an error occurs while retrieving the purchases.
+    """
     try:
         return await PurchaseService.get_null_delivery_dates()
     except Exception as e:
